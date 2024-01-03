1cc401c4e3236eea0052ffb5a2351ce728b02319
Phillip Mugisa <phillipmugisa4@gmail.com>
2024-01-03 11:03:42 +0300

    disposal board complete

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     var currentCalc = "spitzer"
 
+    const STATE = {
+        loan_amount_field: 1000,
+        several_months: 33,
+        annual_interest: 4.32,
+        foreign_exchange: 1.85,
+        monthly_payment_field: 32.193,
+        calculator: "Spitzer"
+    }
+
     const resetCalcs = () => {
         document.querySelector("#equal_fund_calc").classList.remove("hidden")
         document.querySelector("#spitzer_calc").classList.remove("hidden")
@@ -36,29 +45,6 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     })
 
-    document.querySelector("#calc_previous").addEventListener("click", () => {
-        let active = document.querySelector("#calc_input .sm-grid")
-        let previous = active.previousElementSibling
-        if (previous != null) {
-            active.classList.remove("sm-grid")
-            active.classList.add("sm-hidden")
-            
-            previous.classList.remove("sm-hidden")
-            previous.classList.add("sm-grid")
-        }
-    })
-    document.querySelector("#calc_next").addEventListener("click", () => {
-        let active = document.querySelector("#calc_input .sm-grid")
-        let next = active.nextElementSibling
-        if (next != null) {
-            active.classList.add("sm-hidden")
-            active.classList.remove("sm-grid")
-    
-            next.classList.remove("sm-hidden")
-            next.classList.add("sm-grid")
-        }
-    })
-
     const updateFormUI = (computedVal, id) => {
 
         if ("loan_amount_ball" === id) {
@@ -81,6 +67,139 @@ document.addEventListener("DOMContentLoaded", () => {
             document.querySelector("#monthly_payment_field").value = (computedVal * 200).toFixed(2)
         }
     }
+
+    const showCalc = () => {
+        document.querySelector("#for_board").classList.remove("grid")
+        document.querySelector("#disposal_board").classList.remove("grid")
+        document.querySelector("#for_board").classList.add("hidden")
+        document.querySelector("#disposal_board").classList.add("hidden")
+    
+    
+        document.querySelector("#for_calc").classList.add("grid")
+        document.querySelector("#calc_input").classList.add("grid")
+        document.querySelector("#for_calc").classList.remove("hidden")
+        document.querySelector("#calc_input").classList.remove("hidden")
+
+        
+        document.querySelector("#loan_amount_field").value = STATE.loan_amount_field
+        document.querySelector("#several_months").value = STATE.several_months
+        document.querySelector("#foreign_exchange").value = STATE.foreign_exchange
+        document.querySelector("#annual_interest").value = STATE.annual_interest
+        document.querySelector("#monthly_payment_field").value = STATE.monthly_payment_field
+    }
+    
+    const showBoard = () => {
+        // display prebriefs
+        document.querySelector("#calc_type_value").textContent = STATE.calculator
+        document.querySelector("#loan_amount_value").textContent = STATE.loan_amount_field
+        document.querySelector("#payment_period_value").textContent = STATE.several_months
+        document.querySelector("#annual_interest_value").textContent = `${STATE.annual_interest}%`
+        document.querySelector("#indexation_value").textContent = `${STATE.foreign_exchange}%`
+
+        
+        // update board
+        let disposal_board = document.querySelector("#disposal_board")
+        disposal_board.querySelector("tbody").innerHTML = ""
+        let state_copy = {...STATE}
+        let totals = {
+            monthlyRefund : 0,
+            monthly_payment_field : 0,
+            IndexComponentOnInterestRate : 0,
+            paymentInterestComponent : 0,
+            IndexComponentOnFund : 0,
+            paidFundComponent : 0,
+        }
+        for (let i = 1; i <= STATE.several_months; i++) {
+            let tableRow = document.createElement("tr")
+            state_copy.paymentInterestComponent = state_copy.loan_amount_field * toMPercentage(state_copy.annual_interest)
+
+            state_copy.loan_amount_field = parseFloat((state_copy.loan_amount_field + state_copy.paymentInterestComponent - state_copy.monthly_payment_field))
+
+
+            state_copy.IndexComponentOnInterestRate = (toMPercentage(state_copy.foreign_exchange) * state_copy.paymentInterestComponent * i)
+
+            state_copy.paidFundComponent = (state_copy.monthly_payment_field - state_copy.paymentInterestComponent)
+            state_copy.IndexComponentOnFund = (toMPercentage(state_copy.foreign_exchange) * state_copy.paidFundComponent * i)
+
+            state_copy.monthlyRefund = (state_copy.IndexComponentOnFund + state_copy.IndexComponentOnInterestRate + state_copy.monthly_payment_field)
+
+
+            
+            totals.monthlyRefund = (totals.monthlyRefund + state_copy.monthlyRefund)
+            totals.monthly_payment_field = (totals.monthly_payment_field + state_copy.monthly_payment_field)
+            totals.IndexComponentOnInterestRate = (totals.IndexComponentOnInterestRate + state_copy.IndexComponentOnInterestRate)
+            totals.paymentInterestComponent = (totals.paymentInterestComponent + state_copy.paymentInterestComponent)
+            totals.IndexComponentOnFund = (totals.IndexComponentOnFund + state_copy.IndexComponentOnFund)
+            totals.paidFundComponent = (totals.paidFundComponent + state_copy.paidFundComponent)
+            
+
+            tableRow.innerHTML = `
+                <td>${i}</td>
+                <td>${state_copy.loan_amount_field.toFixed(2)}</td>
+                <td>${state_copy.monthlyRefund.toFixed(2)}</td>
+                <td>${state_copy.monthly_payment_field.toFixed(2)}</td>
+                <td>${state_copy.IndexComponentOnInterestRate.toFixed(2)}</td>
+                <td>${state_copy.paymentInterestComponent.toFixed(2)}</td>
+                <td>${state_copy.IndexComponentOnFund.toFixed(2)}</td>
+                <td>${state_copy.paidFundComponent.toFixed(2)}</td>
+            `
+            disposal_board.querySelector("tbody").appendChild(tableRow)
+
+        }
+
+        
+        let tableRow = document.createElement("tr")
+        tableRow.innerHTML = `
+            <td>0</td>
+            <td>${STATE.loan_amount_field}</td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+        `
+        disposal_board.querySelector("tbody").prepend(tableRow)
+
+        // show totals in first row
+        let totalstableRow = document.createElement("tr")
+        totalstableRow.classList.add("totals")
+        totalstableRow.innerHTML = `
+            <td></td>
+            <td></td>
+            <td>${totals.monthlyRefund.toFixed(2)}</td>
+            <td>${totals.monthly_payment_field.toFixed(2)}</td>
+            <td>${totals.IndexComponentOnInterestRate.toFixed(2)}</td>
+            <td>${totals.paymentInterestComponent.toFixed(2)}</td>
+            <td>${totals.IndexComponentOnFund.toFixed(2)}</td>
+            <td>${totals.paidFundComponent.toFixed(2)}</td>
+        `
+        disposal_board.querySelector("tbody").prepend(totalstableRow)
+
+
+        document.querySelector("#for_calc").classList.remove("grid")
+        document.querySelector("#calc_input").classList.remove("grid")
+        document.querySelector("#for_calc").classList.add("hidden")
+        document.querySelector("#calc_input").classList.add("hidden")
+    
+        
+        document.querySelector("#for_board").classList.add("grid")
+        document.querySelector("#disposal_board").classList.add("grid")
+        document.querySelector("#for_board").classList.remove("hidden")
+        document.querySelector("#disposal_board").classList.remove("hidden")
+    }
+
+    let disposal_board_activator = document.querySelector("#disposal_board_activator")
+    disposal_board_activator.addEventListener("click", () => {
+        if (document.querySelector("#calc_input").classList.contains("grid")) {
+            showBoard()
+            disposal_board_activator.textContent = "Return to Calculator"
+        } else if (document.querySelector("#disposal_board").classList.contains("grid")) {
+            showCalc()
+            disposal_board_activator.textContent = "Disposal Board"
+        }
+    })
+
 
     document.querySelectorAll(".calc-group .ball").forEach(elem => {
         elem.draggable = true
@@ -115,6 +234,8 @@ document.addEventListener("DOMContentLoaded", () => {
         .addEventListener("change", () => makeCalculation())
     
 
+    const toMPercentage = value => value / (12 * 100)
+
     const makeCalculation = () => {
         updateRangeUI()
         
@@ -124,22 +245,33 @@ document.addEventListener("DOMContentLoaded", () => {
         annual_interest = document.querySelector("#annual_interest")
         monthly_payment_field = document.querySelector("#monthly_payment_field")
 
-        let R = (annual_interest.value / 100 / 12 )
+        let R = toMPercentage(parseFloat(annual_interest.value)) - (toMPercentage(parseFloat(foreign_exchange.value)) * toMPercentage(parseFloat(annual_interest.value)))
         if (currentCalc === "spitzer") {
-            let ffm = ((1 / several_months.value) + (R * 1) / 2 * ((2 * several_months.value) + 1) / several_months.value)
+            STATE.calculator = "spitzer"
+            let ffm = ((1 / parseFloat(several_months.value)) + (R * 1) / 2 * ((2 * parseFloat(several_months.value)) + 1) / parseFloat(several_months.value))
             if (document.querySelector("#monthly_payment_selector").checked) {
-                let Principle = loan_amount_field.value
-                let monthlyPayment = (Principle * ffm) * (1 + foreign_exchange.value /100)
-                monthly_payment_field.value = monthlyPayment.toFixed()
+                let Principle = parseFloat(loan_amount_field.value) + (toMPercentage(parseFloat(foreign_exchange.value)) * parseFloat(loan_amount_field.value))
+                let monthlyPayment = Principle * ffm
+                monthly_payment_field.value = monthlyPayment.toFixed(2)
     
             } else if (document.querySelector("#load_amount_selector").checked) {
-                let monthlyPayment = monthly_payment_field.value 
+                let monthlyPayment = parseFloat(monthly_payment_field.value) 
                 let Principle = monthlyPayment / ffm
-                loan_amount_field.value = Principle.toFixed()
+                loan_amount_field.value = Principle.toFixed(2)
             }
         } else if (currentCalc === "equal_fund") {
-            let monthlyPayment = loan_amount_field.value / several_months.value  + (loan_amount_field.value * R )
-            monthly_payment_field.value = monthlyPayment.toFixed()
+            STATE.calculator = "Equal Fund"
+            let monthlyPayment = parseFloat(loan_amount_field.value) / parseFloat(several_months.value)  + (parseFloat(loan_amount_field.value) * R )
+            monthly_payment_field.value = monthlyPayment.toFixed(2)
+        }
+
+        if (parseFloat(loan_amount_field.value) > 0 & parseFloat(several_months.value) > 0 & parseFloat(foreign_exchange.value) > 0 & parseFloat(annual_interest.value) > 0 & parseFloat(monthly_payment_field.value) > 0) {
+            document.querySelector("#disposal_board_activator").disabled = false
+            STATE.loan_amount_field = parseFloat(loan_amount_field.value)
+            STATE.several_months = parseFloat(several_months.value)
+            STATE.foreign_exchange = parseFloat(foreign_exchange.value)
+            STATE.annual_interest = parseFloat(annual_interest.value)
+            STATE.monthly_payment_field = parseFloat(monthly_payment_field.value)
         }
         
         updateRangeUI()       
@@ -171,4 +303,30 @@ document.addEventListener("DOMContentLoaded", () => {
         elem.style.bottom = `${value}%`
         elem.style.transform = `translate(-50%, ${value}%)`
     }
+
+    
+    // document.querySelector("#disposal_board_activator").click()
+    document.querySelectorAll("td").forEach(elem => {
+        elem.addEventListener("click", e => {
+            if (!e.ctrlKey) {
+                document.querySelectorAll("td.active").forEach(elem => elem.classList.remove("active"))
+            }
+            elem.classList.toggle("active")
+
+            window.addEventListener("keyup", e => {
+                if (e.key == "Escape") {
+                    document.querySelectorAll("td.active").forEach(elem => elem.classList.remove("active"))
+                }
+            })
+            
+            // elem.addEventListener("mousedown", e => {
+            //     document.querySelectorAll("td").forEach(elem => {
+            //         elem.addEventListener("mouseover", e => {
+            //             console.log(e)
+            //             elem.classList.add("active")
+            //         })
+            //     })
+            // })
+        })
+    })
 })
